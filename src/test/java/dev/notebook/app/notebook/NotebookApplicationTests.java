b5ff9a46d1d7190142d939da23a45424dc953da5
Dmitrii <zinchukdimaa@gmail.com>
2020-05-08 00:54:02 +0300

    initial commit

---
+++ src/test/java/dev/notebook/app/notebook/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.notebook.app.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
